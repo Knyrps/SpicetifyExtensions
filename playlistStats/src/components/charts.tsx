bd89b98dc3eaf5acd0fd6f15e7894079257326d6
Valentin Pommée <valentin.pommee@web.de>
2024-10-12 03:20:00 +0200

    Revert "Auxiliary commit to revert individual files from 4b37c18f7fd64ccded44963993a5ee2394a37932"
    This reverts commit 70fc1b4e04966338d2a3164baa8d89c2e18928db.

--- playlistStats/src/components/charts.tsx
+++ playlistStats/src/components/charts.tsx
@@ -12,7 +12,6 @@ import {
     CollaborationModel,
     getCollaborators,
 } from "../helpers/playlistHelper";
-import { shift } from "../helpers/utilHelper";
 
 const PieChart = React.memo(() => {
     const [loading, setLoading] = useState(true);
@@ -41,78 +40,59 @@ const PieChart = React.memo(() => {
         return <div>No data available</div>;
     }
 
-    const colors =
-        data.length > 1
-            ? generateColorFade(
-                  colorPalette.GetColor("subtext"),
-                  colorPalette.GetColor("button-secondary"),
-                  data.length
-              )
-            : [colorPalette.GetColor("subtext")];
-    const secondaryColors = shift(colors);
+    const colors = generateColorFade(
+        colorPalette.GetColor("subtext"),
+        colorPalette.GetColor("button-secondary"),
+        data.length
+    );
 
     return (
-        <>
-            <style>
-                {data.map((d, index) => {
-                    return `.recharts-sector[name="${d.name}"] { stroke: ${secondaryColors[index]}; }`;
-                })}
-            </style>
-            <ResponsiveContainer width="100%" height="100%">
-                <RechartsPieChart
-                    margin={{ top: 5, bottom: 5, left: 5, right: 5 }}
+        <ResponsiveContainer width="100%" height="100%">
+            <RechartsPieChart margin={{ top: 0, bottom: 0, left: 0, right: 0 }}>
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    nameKey="name"
+                    startAngle={90}
+                    endAngle={-270}
+                    paddingAngle={0}
+                    outerRadius="100%"
+                    innerRadius="70%"
+                    animationBegin={100}
+                    animationDuration={500}
+                    animationEasing="ease-out"
                 >
-                    <Pie
-                        data={data}
-                        dataKey="value"
-                        nameKey="name"
-                        startAngle={90}
-                        endAngle={-270}
-                        paddingAngle={0}
-                        outerRadius="100%"
-                        innerRadius="70%"
-                        animationBegin={100}
-                        animationDuration={500}
-                        animationEasing="ease-out"
-                    >
-                        {data.map((_, index) => (
-                            <Cell
-                                style={{
-                                    stroke: `1% var(--spice-${secondaryColors[index]})`,
-                                }}
-                                key={`cell-${index}`}
-                                fill={colors[index]}
-                            />
-                        ))}
-                    </Pie>
-                    <Legend
-                        verticalAlign="middle"
-                        layout="vertical"
-                        iconType="circle"
-                    />
-                    <Tooltip
-                        wrapperClassName="pieChart-tooltip"
-                        wrapperStyle={{
-                            backgroundColor: "transparent",
-                            whiteSpace: "nowrap",
-                            border: "none",
-                        }}
-                        contentStyle={{
-                            backgroundColor: "var(--spice-main-secondary)",
-                            border: "none",
-                            borderRadius: "8px",
-                        }}
-                        labelStyle={{
-                            color: "var(--spice-text)",
-                        }}
-                        itemStyle={{
-                            color: "var(--spice-subtext)",
-                            fontWeight: "400",
-                        }}
-                    />
-                </RechartsPieChart>
-            </ResponsiveContainer>
-        </>
+                    {data.map((_, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index]} />
+                    ))}
+                </Pie>
+                <Legend
+                    verticalAlign="middle"
+                    layout="vertical"
+                    iconType="circle"
+                />
+                <Tooltip
+                    wrapperClassName="pieChart-tooltip"
+                    wrapperStyle={{
+                        backgroundColor: "transparent",
+                        whiteSpace: "nowrap",
+                        border: "none",
+                    }}
+                    contentStyle={{
+                        backgroundColor: "var(--spice-main-secondary)",
+                        border: "none",
+                        borderRadius: "8px",
+                    }}
+                    labelStyle={{
+                        color: "var(--spice-text)",
+                    }}
+                    itemStyle={{
+                        color: "var(--spice-subtext)",
+                        fontWeight: "400",
+                    }}
+                />
+            </RechartsPieChart>
+        </ResponsiveContainer>
     );
 });
 
